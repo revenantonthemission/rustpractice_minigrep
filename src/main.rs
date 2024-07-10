c94b34c0c5d21028a87b12afd8d594d0e8406435
revenantonthemission <corpseonthemission@icloud.com>
2024-07-10 12:18:22 +0900

    run 함수로 실행 파트를 분리

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     //cargo run [options] [-- args]이기 때문에 --는 따로 처리하지 않아도 env::args().collect()를 통해 수집될 수 있다.
@@ -16,9 +17,19 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    //fs::read_to_string 함수를 사용해 파일을 열고 std::io::Result<String>을 반환한다. 실패시 expect를 사용한다.
+    //실행 파트를 run으로 분리.
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+//1. 유닛 타입()을 반환하는 함수에서 Result<(), Box<dyn Error>>를 반환하는 함수로 변경한다. 이때 ()는 Ok(())다.
+//2. Box<dyn Error>는 트레이트 객체로, Error 트레이트를 구현한 어떤 타입을 반환한다.
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    //fs::read_to_string 함수를 사용해 파일을 열고 std::io::Result<String>을 반환한다. ? 연산자를 통해 현재의 함수로부터 에러 값을 받을 수 있다.
     let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+        fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
 }
