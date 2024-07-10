cc47bfef5c1fe0322803a1efd6a3f710203d1e94
revenantonthemission <corpseonthemission@icloud.com>
2024-07-10 12:23:29 +0900

    라이브러리로 코드 쪼개기

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+//Result를 통해 에러 처리
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
+}
+
+//1. 유닛 타입()을 반환하는 함수에서 Result<(), Box<dyn Error>>를 반환하는 함수로 변경한다. 이때 ()는 Ok(())다.
+//2. Box<dyn Error>는 트레이트 객체로, Error 트레이트를 구현한 어떤 타입을 반환한다.
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    //fs::read_to_string 함수를 사용해 파일을 열고 std::io::Result<String>을 반환한다. ? 연산자를 통해 현재의 함수로부터 에러 값을 받을 수 있다.
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 //커맨드 라인 인수로 전달된 값들을 읽기 위해 std::env::args 함수를 사용한다.
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+//라이브러리 크레이트의 코드를 가져온다.
+use minigrep::Config;
 
 fn main() {
     //cargo run [options] [-- args]이기 때문에 --는 따로 처리하지 않아도 env::args().collect()를 통해 수집될 수 있다.
@@ -18,35 +19,8 @@ fn main() {
     println!("In file {}", config.file_path);
 
     //실행 파트를 run으로 분리.
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
-    }
-}
-
-//1. 유닛 타입()을 반환하는 함수에서 Result<(), Box<dyn Error>>를 반환하는 함수로 변경한다. 이때 ()는 Ok(())다.
-//2. Box<dyn Error>는 트레이트 객체로, Error 트레이트를 구현한 어떤 타입을 반환한다.
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    //fs::read_to_string 함수를 사용해 파일을 열고 std::io::Result<String>을 반환한다. ? 연산자를 통해 현재의 함수로부터 에러 값을 받을 수 있다.
-    let contents =
-        fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-//Result를 통해 에러 처리
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
     }
 }
