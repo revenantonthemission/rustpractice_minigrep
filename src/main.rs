484ce3f9ad057c8943257c65259f2068ca56e48f
revenantonthemission <corpseonthemission@icloud.com>
2024-07-10 11:26:35 +0900

    커맨드 입력 변수 저장하기

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+//커맨드 라인 인수로 전달된 값들을 읽기 위해 std::env::args 함수를 사용한다.
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    //cargo run [options] [-- args]이기 때문에 --는 따로 처리하지 않아도 env::args().collect()를 통해 수집될 수 있다.
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
 }
